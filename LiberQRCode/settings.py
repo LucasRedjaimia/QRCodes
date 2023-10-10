713485b3a33803a54910cbc8430f0c5e51b0bb72
redjaimia lucas <lucas.redjaimia@epitech.eu>
2023-10-10 15:48:22 +0200

    config

--- LiberQRCode/settings.py
+++ LiberQRCode/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.contrib import staticfiles
 from django.core.management import templates
 from django.template.context_processors import media
 
@@ -39,7 +38,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -109,18 +107,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if 'DYNO' in os.environ:
-    import django_heroku
-    django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Login & Logout URLs
 LOGIN_URL = '/login/'
